96ff6694183fa379e096638dcd65e1b15cd58b0d
HamHamFonFon <balistik.fonfon@gmail.com>
2018-04-25 19:30:06 +0200

    Fix GetTodayImage

--- Services/GetTodayImage.php
+++ Services/GetTodayImage.php
@@ -9,7 +9,7 @@
 namespace HamhamFonfon\Astrobin\Services;
 
 use HamhamFonfon\Astrobin\AbstractWebService;
-use HamhamFonfon\Astrobin\Exceptions\AstrobinResponseExceptions;
+use HamhamFonfon\Astrobin\Exceptions\WsResponseException;
 use HamhamFonfon\Astrobin\Response\Image;
 use HamhamFonfon\Astrobin\Response\Today;
 use HamhamFonfon\Astrobin\WsInterface;
@@ -27,7 +27,8 @@ class getTodayImage extends AbstractWebService implements WsInterface
 
 
     /**
-     * @throws \HamhamFonfon\Astrobin\Exceptions\WsResponseException
+     * @throws WsResponseException
+     * @throws \HamhamFonfon\Astrobin\Exceptions\WsException
      * @throws \ReflectionException
      */
     public function getTodayImage()
@@ -40,8 +41,8 @@ class getTodayImage extends AbstractWebService implements WsInterface
         if ($today->format(self::FORMAT_DATE_ASTROBIN) == $astrobinToday->date) {
 
             $urlTest = AbstractWebService::ASTROBIN_URL . substr($astrobinToday->image, strrpos($astrobinToday->image, '/v1/')+strlen('/v1/'));
-            dump($urlTest);
-            dump(file_get_contents($urlTest));
+//            dump($urlTest);
+//            dump(file_get_contents($urlTest));
 //            if (preg_match('/\/([\d]+)/', $astrobinToday->image, $matches)) {
 //                $imageId = $matches[1];
 //                dump(AstrobinWebService::ASTROBIN_URL . $astrobinToday->image);
@@ -49,24 +50,23 @@ class getTodayImage extends AbstractWebService implements WsInterface
 //                dump($sndRawCall);
 //            }
         }
-
-        die();
     }
 
 
     /**
      * @param array $params
-     * @return Image
-     * @throws \AppBundle\Astrobin\Exceptions\astrobinException
+     * @return mixed
+     * @throws WsResponseException
+     * @throws \HamhamFonfon\Astrobin\Exceptions\WsException
      */
     public function callWs($params = [])
     {
         /** @var  $rawResp */
         $rawResp = $this->call(self::END_POINT, parent::METHOD_GET, $params);
         if (!isset($rawResp->objects) || 0 == $rawResp->meta->total_count) {
-            throw new AstrobinResponseExceptions("Response from Astrobin is empty");
+            throw new WsResponseException("Response from Astrobin is empty");
         }
-        return $rawResp->objects;
+        return $this->responseWs($rawResp->objects);
     }
 
 
