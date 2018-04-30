600b9023be6f3c8c252ca0965a2c7f9ef524436a
HamHamFonFon <balistik.fonfon@gmail.com>
2018-04-30 22:02:02 +0200

    Entity Collection

--- src/Response/Collection.php
+++ src/Response/Collection.php
@@ -1,31 +1,63 @@
 <?php
 
 namespace Astrobin\Response;
+use Astrobin\AbstractWebService;
+use Traversable;
 
 /**
  * Class Collection
  * @package Astrobin\Response
  */
-class Collection
+class Collection extends AbstractWebService implements \IteratorAggregate
 {
 
     public $id;
+    public $name;
+    public $description;
+    public $user;
+    public $date_created;
+    public $date_updated;
+    public $listImages;
+
 
     /**
-     * @param $images
-     * @throws \Astrobin\Exceptions\WsResponseException
-     * @throws \ReflectionException
+     * @return ImageIterator|Traversable
      */
-//    public function setImages($images)
-//    {
-//        $listImages = [];
-//        foreach ($images as $image) {
-//            $astrobinImage = new Image();
-//            $astrobinImage->fromObj($image);
-//            $listImages[] = $astrobinImage;
-//        }
-//        $this->images = $listImages;
-//    }
+    public function getIterator()
+    {
+        return new ImageIterator($this->listImages);
+    }
+
+    /**
+     * @param Image $image
+     */
+    public function add(Image $image)
+    {
+        $this->listImages[] = $image;
+    }
+
+
+    /**
+     * @param $date_created
+     * @return $this
+     */
+    public function setDateCreated($date_created)
+    {
+        $this->date_created = \DateTime::createFromFormat('Y-m-d', $date_created);
+        return $this;
+    }
+
+
+    /**
+     * @param $date_updated
+     * @return $this
+     */
+    public function setDateUpdated($date_updated)
+    {
+        $this->date_updated = \DateTime::createFromFormat('Y-m-d', $date_updated);
+        return $this;
+    }
+
 
 
 }
