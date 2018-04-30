35b044f4e0ce4d22f4def2c4c6433bc4164ed559
HamHamFonFon <balistik.fonfon@gmail.com>
2018-04-30 22:46:25 +0200

    Add response ListCollection

---
+++ src/Response/ListCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Astrobin\Response;
+
+use Astrobin\Response\Iterators\CollectionIterator;
+
+/**
+ * Class ListCollection
+ * @package Astrobin\Response
+ */
+class ListCollection extends AbstractResponse implements \IteratorAggregate
+{
+
+    private $listCollection;
+
+    public function getIterator()
+    {
+        return new CollectionIterator($this->listCollection);
+    }
+
+    /**
+     * @param $collection
+     */
+    public function add($collection)
+    {
+        $this->listCollection[] = $collection;
+    }
+}
