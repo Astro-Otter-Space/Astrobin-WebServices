8ec028d634a17378113878233bcb6360e25d95bc
HamHamFonFon <balistik.fonfon@gmail.com>
2018-04-30 19:33:52 +0200

    Adding Iterator classes

---
+++ src/Response/ListImages.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephane
+ * Date: 30/04/18
+ * Time: 19:23
+ */
+
+namespace Astrobin\Response;
+
+use Traversable;
+
+/**
+ * Class ListImages
+ * @package Astrobin\Response
+ */
+class ListImages implements \IteratorAggregate
+{
+
+    private $listImages = [];
+    private $count = 0;
+
+    /**
+     * @return ImageIterator|Traversable
+     */
+    public function getIterator()
+    {
+        return new ImageIterator($this->listImages);
+    }
+
+    /**
+     * @param $image
+     */
+    public function add($image)
+    {
+        $this->listImages[$this->count++] = $image;
+    }
+}
