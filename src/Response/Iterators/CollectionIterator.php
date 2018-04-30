4081292fc098b4081450010d7326a6e37e27ca31
HamHamFonFon <balistik.fonfon@gmail.com>
2018-04-30 22:42:51 +0200

    Add collecitonIterator

---
+++ src/Response/Iterators/CollectionIterator.php
@@ -0,0 +1,74 @@
+<?php
+namespace Astrobin\Response\Iterators;
+
+use Traversable;
+
+/**
+ * Class CollectionIterator
+ * @package Astrobin\Response\Iterators
+ */
+class CollectionIterator implements \Iterator
+{
+    private $var = [];
+
+
+    /**
+     * CollectionIterator constructor.
+     * @param $array
+     */
+    public function __construct($array)
+    {
+        if (is_array($array)) {
+            $this->var = $array;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function current()
+    {
+        $var = current($this->var);
+        return $var;
+    }
+
+
+    /**
+     * @return mixed|void
+     */
+    public function next()
+    {
+        $var = next($this->var);
+        return $var;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function key()
+    {
+        $var = key($this->var);
+        return $var;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        $key = key($this->var);
+        $var = (!is_null($key) && FALSE !== $key);
+        return $var;
+    }
+
+    /**
+     * @return mixed|void
+     */
+    public function rewind()
+    {
+        $var = reset($this->var);
+        return $var;
+    }
+
+}
