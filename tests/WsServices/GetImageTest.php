bbdd06bb039e2eb14a2ca92d6785da976ce2a390
HamHamFonFon <balistik.fonfon@gmail.com>
2018-05-05 01:35:56 +0200

    Exeplte TU

--- tests/WsServices/GetImageTest.php
+++ tests/WsServices/GetImageTest.php
@@ -7,39 +7,62 @@ use Astrobin\Services\GetImage;
 class GetImageTest extends PHPUnit_Framework_TestCase
 {
 
+    /** @var GetImage */
+    private $client;
+
+    CONST FAKE_KEY = '3524e6ee81749ea19a1ed0f14c5390efb4ac578f';
+
+    CONST FAKE_SECRET = '6f0a67f7aeb93cbce4addec000fca9991876df63';
+
     /**
      *
      */
     public function setUp()
     {
         parent::setUp();
+        $this->client = new GetImage(self::FAKE_KEY, self::FAKE_SECRET);
     }
 
+//    /**
+//     * Test Call API without Token
+//     *
+//     * @throws ReflectionException
+//     * @throws \Astrobin\Exceptions\WsException
+//     * @throws \Astrobin\Exceptions\WsResponseException
+//     */
+//    public function testGetImageByIdWithoutToken()
+//    {
+//        $id = rand(1, 1000);
+//        $response = $client->getImageById($id);
+//
+//        $this->assertInstanceOf(\Astrobin\Exceptions\WsException::class, $response,  '');
+//    }
+
+
     /**
-     * Test Call API without Token
-     *
      * @throws ReflectionException
      * @throws \Astrobin\Exceptions\WsException
      * @throws \Astrobin\Exceptions\WsResponseException
      */
-    public function testGetImageByIdWithoutToken()
-    {
-        $client = new GetImage(null, null);
-        $id = rand(1, 1000);
-        $response = $client->getImageById($id);
-
-        $this->assertInstanceOf(\Astrobin\Exceptions\WsException::class, $response,  '');
-    }
-
-
     public function testGetImageById()
     {
         $id = 341955;
-        $client = new GetImage('3524e6ee81749ea19a1ed0f14c5390efb4ac578f', '6f0a67f7aeb93cbce4addec000fca9991876df63');
-        $response = $client->getImageById($id);
+        $response = $this->client->getImageById($id);
 
         $this->assertInstanceOf(\Astrobin\Response\Image::class, $response);
         $this->assertClassHasAttribute('title', \Astrobin\Response\Image::class);
     }
 
+
+    /**
+     * @throws ReflectionException
+     * @throws \Astrobin\Exceptions\WsException
+     * @throws \Astrobin\Exceptions\WsResponseException
+     */
+    public function testGetImageWithoutId()
+    {
+        $response = $this->client->getImageById(null);
+
+        $this->assertNull($response);
+    }
 }
