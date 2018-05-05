5e92ec1966e35d881d1b6991f0c46d79929ad729
HamHamFonFon <balistik.fonfon@gmail.com>
2018-05-05 16:47:35 +0200

    Add date uploaded on Image

--- src/Response/Image.php
+++ src/Response/Image.php
@@ -11,9 +11,20 @@ class Image extends AbstractResponse
     public $title;
     public $subjects;
     public $description;
+    public $uploaded;
     public $url_gallery;
     public $url_thumb;
     public $url_regular;
     public $url_hd;
     public $user;
+
+    /**
+     * @param $uploaded
+     * @return $this
+     */
+    public function setUploaded($uploaded)
+    {
+        $this->uploaded = \DateTime::createFromFormat(DATE_ATOM, $uploaded);
+        return $this;
+    }
 }
