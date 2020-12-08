f98c9f878c4cb4a2e50e78d3b1ac1ae5c9fb5b04
Stephane Meaudre <smeaudre@kaliop.com>
2020-12-08 15:00:09 +0100

    Add docker stack

--- tests/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require './src/AbstractWebService.php';
-require './src/Services/GetImage.php';
-require './src/Services/WsInterface.php';
-
-$imageWs = new \AstrobinWs\Services\GetImage();
-$image = $imageWs->getById(341955);
