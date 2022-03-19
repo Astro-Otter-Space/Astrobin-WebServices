7f7ff39a6fe58b129b8e61fddc49df4e9e7ffa28
Stephane Meaudre <smeaudre@kaliop.com>
2022-03-19 20:32:28 +0100

    Fix type-int properties

--- src/Response/Image.php
+++ src/Response/Image.php
@@ -11,16 +11,16 @@ namespace AstrobinWs\Response;
 final class Image extends AbstractResponse implements AstrobinResponse
 {
     public string $title;
-    public string $subjects;
-    public string $description;
+    public array $subjects;
+    public ?string $description;
     public string $uploaded;
-    public string $url_gallery;
-    public string $url_thumb;
-    public string $url_regular;
-    public string $url_hd;
+    public ?string $url_gallery;
+    public ?string $url_thumb;
+    public ?string $url_regular;
+    public ?string $url_hd;
     public string $user;
-    public string $url_histogram;
-    public string $url_skyplot;
+    public ?string $url_histogram;
+    public ?string $url_skyplot;
 
     /**
      * @return bool|\DateTime
