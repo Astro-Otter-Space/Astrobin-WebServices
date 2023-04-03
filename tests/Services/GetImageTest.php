7783213832f111c764b3d5834b603cac7a86656c
smeaudre <smeaudre@kaliop.com>
2023-04-03 15:26:24 +0200

    Typo

---
+++ tests/Services/GetImageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Services;
+
+use AstrobinWs\Services\GetImage;
+use PHPUnit\Framework\TestCase;
+
+class GetImageTest extends TestCase
+{
+
+    public function testGetImageById()
+    {
+
+    }
+}
