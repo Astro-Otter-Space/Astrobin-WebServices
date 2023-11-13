c1683fb22a0845ab2df44e095a311d0fc7dd1f0e
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-11-13 09:33:15 +0100

    Test code

--- src/test.php
+++ src/test.php
@@ -5,34 +5,33 @@ declare(strict_types=1);
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 #Exception
-include __DIR__ . './../src/Exceptions/WsException.php';
-include __DIR__ . './../src/Exceptions/WsResponseException.php';
+include __DIR__ . '/Exceptions/WsException.php';
+include __DIR__ . '/Exceptions/WsResponseException.php';
 
 # WS
-include __DIR__ . './../src/GuzzleSingleton.php';
-include __DIR__ . './../src/AbstractWebService.php';
-include __DIR__ . './../src/Services/WsInterface.php';
-include __DIR__ . './../src/Services/GetImage.php';
-include __DIR__ . './../src/Services/GetUser.php';
+include __DIR__ . '/GuzzleSingleton.php';
+include __DIR__ . '/AbstractWebService.php';
+include __DIR__ . '/Services/WsInterface.php';
+include __DIR__ . '/Services/GetImage.php';
+//include __DIR__ . '/Services/GetUser.php';
 
 # Response
-include __DIR__ . './../src/Response/DTO/AstrobinResponse.php';
-include __DIR__ . './../src/Response/AbstractResponse.php';
-include __DIR__ . './../src/Response/DTO/Collection/ListImages.php';
-include __DIR__ . './../src/Response/DTO/Collection/ListCollection.php';
-include __DIR__ . './../src/Response/DTO/Collection/ListToday.php';
-include __DIR__ . './../src/Response/DTO/Item/Image.php';
-include __DIR__ . './../src/Response/DTO/Item/User.php';
-include __DIR__ . './../src/Response/DTO/Item/Collection.php';
+include __DIR__ . '/Response/DTO/AstrobinResponse.php';
+include __DIR__ . '/Response/AbstractResponse.php';
+//include __DIR__ . '/Response/DTO/Collection/ListImages.php';
+//include __DIR__ . '/Response/DTO/Collection/ListCollection.php';
+//include __DIR__ . '/Response/DTO/Collection/ListToday.php';
+include __DIR__ . '/Response/DTO/Item/Image.php';
+//include __DIR__ . '/Response/DTO/Item/User.php';
+//include __DIR__ . '/Response/DTO/Item/Collection.php';
 
 $astrobinApiKey = getenv('ASTROBIN_API_KEY');
 $astrobinApiSecret = getenv('ASTROBIN_API_SECRET');
 
-$astrobinWs = new \AstrobinWs\Services\GetCollection($astrobinApiKey, $astrobinApiSecret);
+$astrobinWs = new \AstrobinWs\Services\GetImage($astrobinApiKey, $astrobinApiSecret);
 try {
-    $response = $astrobinWs->getById('25');
-
-    var_dump($response->images->count);
+    $response = $astrobinWs->getById('n4nth8');
+    var_dump($response);
 } catch (\AstrobinWs\Exceptions\WsException|JsonException) {
 }
 
